36093f4385266c24475827f67a38bf734ba15fa1
josephd <josephd@clarkson.edu>
2015-10-29 03:23:22 -0400

    removing some files

--- homework1/homework1/Encryptor.java
+++
@@ -1,9 +0,0 @@
-public abstract class Encryptor {
-	
-	private String encrypted = new String(); // a string to store the enctrypted or decrypted message in 
-	
-	public void textEncrypt(String Input){} 
-	
-	public void textDecrypt(String Input, int key){}
-
-}
